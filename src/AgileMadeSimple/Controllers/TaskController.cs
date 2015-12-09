e3ed2f47eedee5466b25a7c50b716b72f38b7efb
Tyler Lemieux <lemex2004@gmail.com>
2015-12-08 22:23:51 -0500

    added task controller

--- src/AgileMadeSimple/Controllers/TaskController.cs
+++ src/AgileMadeSimple/Controllers/TaskController.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using AgileMadeSimple.Models;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,34 +14,98 @@ namespace AgileMadeSimple.Controllers
     {
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Task> Get()
         {
-            return new string[] { "value1", "value2" };
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                return context.Task.Select(t => t);
+            }
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{taskId}")]
+        public Task Get(int taskId)
         {
-            return "value";
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                return context.Task.Where(t => t.TaskID == taskId).First();
+            }
         }
+
+        [HttpGet("StoryTask/{storyId}")]
+        public IEnumerable<Task> GetStoryTasks(int storyId)
+        {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                return context.Task.Where(t => t.StoryID == storyId).Select(t => t);
+            }
+        }
+
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public Task Post([FromBody]Task task)
         {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                context.Task.Add(task);
+                context.SaveChanges();
+                return task;
+            }
         }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        [HttpPut]
+        public Task Put([FromBody]Task task)
         {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                context.Task.Update(task);
+                context.SaveChanges();
+                return task;
+            }
+        }
+
+        [HttpPut("AddTag/{taskId}/{tagName}")]
+        public void AddTag(int taskId, string tagName)
+        {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                Tag tag = TagHandler.GetTagID(tagName);
+                TaskTag taskTag = new TaskTag();
+                taskTag.TaskID = taskId;
+                taskTag.TagID = tag.TagID;
+            }
+        }
+
+        [HttpDelete("RemoveTag/{taskId}/{tagName}")]
+        public void RemoveTag(int taskId, string tagName)
+        {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                IEnumerable<TaskTag> taskTag =
+                    from tt in context.TaskTag
+                    join t in context.Tag on tt.TagID equals t.TagID
+                    where taskId == tt.TaskID && t.Name == tagName
+                    select tt;
+
+                context.TaskTag.RemoveRange(taskTag);
+                context.SaveChanges();
+            }
         }
 
         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("{taskId}")]
+        public void Delete(int taskId)
         {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                Task task = context.Task.Where(t => t.TaskID == taskId).First();
+                IEnumerable<TaskTag> taskTag = context.TaskTag.Where(tt => tt.TaskID == taskId).Select(tt => tt);
+                context.Task.Remove(task);
+                context.TaskTag.RemoveRange(taskTag);
+                context.SaveChanges();
+            }
         }
     }
 }
