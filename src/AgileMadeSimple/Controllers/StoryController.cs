005d5c82e1e07616d2b490ee13d7e9d6736086a9
Tyler Lemieux <lemex2004@gmail.com>
2015-12-08 22:01:37 -0500

    added story controller and tag handler to handle tag creation/adding new instances of a tag to task/story etc

--- src/AgileMadeSimple/Controllers/StoryController.cs
+++ src/AgileMadeSimple/Controllers/StoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using AgileMadeSimple.Models;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,34 +14,88 @@ namespace AgileMadeSimple.Controllers
     {
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Story> Get()
         {
-            return new string[] { "value1", "value2" };
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                return context.Story.Select(s => s);
+            }
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{storyId}")]
+        public Story Get(int storyId)
         {
-            return "value";
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                return context.Story.Where(s => s.StoryID == storyId).First();
+            }
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public Story Post([FromBody]Story story)
         {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                context.Story.Add(story);
+                context.SaveChanges();
+                return story;
+            }
         }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        [HttpPut]
+        public Story Put([FromBody]Story story)
         {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                context.Update(story);
+                return story;
+            }
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpPut("AddTag/{storyId}/{tagName}")]
+        public void AddTag(int storyId, string tagName)
         {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                Tag tag = TagHandler.GetTagID(tagName);
+                StoryTag storyTag = new StoryTag();
+                storyTag.StoryID = storyId;
+                storyTag.TagID = tag.TagID;
+            }
+        }
+
+        [HttpDelete("RemoveTag/{storyId}/{tagName}")]
+        public void RemoveTag(int storyId, string tagName)
+        {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                IEnumerable<StoryTag> storyTag =
+                    from st in context.StoryTag
+                    join t in context.Tag on st.TagID equals t.TagID
+                    where storyId == st.StoryID && t.Name == tagName
+                    select st;
+
+                context.StoryTag.RemoveRange(storyTag);
+                context.SaveChanges();
+            }
+        }
+
+
+        // DELETE api/values/5
+        [HttpDelete("{storyId}")]
+        public void Delete(int storyId)
+        {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                Story story = context.Story.Where(s => s.StoryID == storyId).First();
+                IEnumerable<StoryTag> storyTag = context.StoryTag.Where(st => st.StoryID == storyId).Select(st => st);
+                context.Story.Remove(story);
+                context.StoryTag.RemoveRange(storyTag);
+                context.SaveChanges();
+            }
         }
     }
 }
---
+++ src/AgileMadeSimple/Models/TagHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgileMadeSimple.Models
+{
+    public static class TagHandler
+    {
+        public static Tag GetTagID(string name)
+        {
+            using (AgileMadeSimpleContext context = new AgileMadeSimpleContext())
+            {
+                Tag tag = context.Tag.Where(t => t.Name == name).FirstOrDefault();
+
+                if (tag == null)
+                {
+                    //Tag doesnt exist, create a new one
+                    tag = new Tag() { Name = name };
+                    context.Add(tag);
+                    context.SaveChanges();
+                }
+
+                return tag;
+            }
+        }
+    }
+}
